8e06a51ba3c34ff38dd48cbcf004c999268ce332
Ashwin02 <ashwinabraham2001@gmail.com>
2025-07-10 15:34:37 +0530

    Very small changes

--- Assets/Scripts/Events/EventService.cs
+++ Assets/Scripts/Events/EventService.cs
@@ -4,19 +4,10 @@ namespace ChestSystem.Events
 {
     public class EventService
     {
-        //public EventController OnChestAdded;
-        //public EventController<int, int> OnChestCollected;
         public EventController<ChestController> OnChestCollected;
-
-        public EventController<int> OnGemsChanged;
-        public EventController<int> OnGoldChanged;
 
         public EventService()
         {
-            //OnChestAdded = new EventController();
-            //OnChestCollected = new EventController<int, int>();
-            OnGemsChanged = new EventController<int>();
-            OnGoldChanged = new EventController<int>();
             OnChestCollected = new EventController<ChestController>();
         }
     }
