e4dcf18d8ca07f014cace426534d15423364c66f
Ashwin02 <ashwinabraham2001@gmail.com>
2025-07-10 15:38:08 +0530

    Re entering accidentally deleted code

--- Assets/Scripts/Events/EventService.cs
+++ Assets/Scripts/Events/EventService.cs
@@ -5,10 +5,16 @@ namespace ChestSystem.Events
     public class EventService
     {
         public EventController<ChestController> OnChestCollected;
+        public EventController<int> OnGemsChanged;
+        public EventController<int> OnGoldChanged;
 
         public EventService()
         {
             OnChestCollected = new EventController<ChestController>();
+            OnGemsChanged = new EventController<int>();
+            OnGoldChanged = new EventController<int>();
         }
+
+        
     }
 }
